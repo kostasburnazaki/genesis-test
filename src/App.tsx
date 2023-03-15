3b6dc46f46bc2ec0648bdc07f38d258fd24602d0
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2023-03-15 21:11:15 +0200

    stage 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 import { client } from './api';
 import { Course } from './types/Course';
-import './App.css';
+import { Loader } from './components/Loader';
+import { Pagination } from './components/Pagination';
+import { Courses } from './components/Courses'
 
 export const App = () => {
-  const [courses, setCourses] = useState<void | Course[]>();
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [coursesPerPage] = useState<number>(5);
+
+  const indexOfLastCourse = currentPage * coursesPerPage;
+  const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
+  const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse)
 
   useEffect(() => {
     client.getCourses()
-      .then(res => {
-        console.log(res);
+      .then(coursesData => {
+        setCourses(coursesData.courses);
+        setLoading(false);
       })
-    .catch(err => console.warn(err))
+      .catch(err => console.warn(err))
   }, []);
 
   return (
-    <div>
-      <h1>Courses</h1>
-      <button className='button is-primary is-small'>Test</button>
+    <>
+      <header className='has-background-light'>
+        <h1 className="
+          title
+          is-size-1
+          has-text-centered
+          has-text-primary
+          py-6
+        ">
+          Courses
+        </h1>
+      </header>
 
-    </div>
+      <main className='has-background-light'>
+        {loading
+          ? <Loader />
+          : <Courses courses={currentCourses} />}
+          <Pagination
+            coursesPerPage={coursesPerPage}
+            totalCourses={courses.length}
+            paginate={setCurrentPage}
+            currentPage={currentPage}
+          />
+      </main>
+
+      <footer>
+      </footer>
+
+    </>
   );
 };
--- src/api.tsx
+++ src/api.tsx
@@ -1,25 +1,27 @@
 import { Course } from "./types/Course";
+import { Token } from "./types/Token";
 
-const API_URL = 'http://api.wisey.app';
+const API_URL = 'https://api.wisey.app';
 const VERSION_URL = '/api/v1';
 const BASE_URL = API_URL + VERSION_URL;
 const COURSES = '/core/preview-courses';
 const AUTH = '/auth/anonymous?platform=subscriptions';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzNmFiMGQ2OC0yZjkzLTRiZGQtYjY3Ni0yZGNkNGNiOTI0MmUiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg4NzQzMDIsImV4cCI6MTY3OTc3NDMwMn0._wj8FF3p2jJSOM1YNcfCRxIFFCXi5R1-oW0rOd59JDo';
 
-type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
 function request<T>(
   url: string,
 ): Promise<any> {
-  const options: RequestInit = {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      Authorization: `Bearer ${token}`,
-    },
-  }
-
-  return fetch(BASE_URL + url, options)
+  return fetch(BASE_URL + AUTH)
+    .then(res => res.json())
+    .then(({ token }: Token) => {
+      const options: RequestInit = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+      return fetch(BASE_URL + url, options)
+        .then(res => res.json())
+    })
 };
 
 export const client = {
